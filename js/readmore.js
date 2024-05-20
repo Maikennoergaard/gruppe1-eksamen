8a1e952b0523e0c32d491547771341ca00c3d3bc
ceciliatorp <eaa23ceth@students.eaaa.dk>
2024-05-21 01:05:21 +0200

    ny bag om

--- js/readmore.js
+++ js/readmore.js
@@ -1,16 +1,35 @@
 'use strict';
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
+document.addEventListener("DOMContentLoaded", function() {
+  var foldReplyElements = document.querySelectorAll(".fold_reply");
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+  foldReplyElements.forEach(function(foldReply) {
+      foldReply.addEventListener("click", function() {
+          var targetClass = foldReply.getAttribute("data-target");
+          var targetElement = document.querySelector(targetClass);
+
+          // gemmer de andre reply sektioner
+          document.querySelectorAll(".reply").forEach(function(reply) {
+              if (reply !== targetElement) {
+                  reply.classList.remove("visible");
+              }
+          });
+
+          
+          targetElement.classList.toggle("visible");
+      });
   });
-}
+});
+
+// Ekstar CSS til styling
+const style = document.createElement('style');
+style.innerHTML = `
+    .reply {
+        display: none;
+    }
+    .reply.visible {
+        display: block;
+    }
+`;
+document.head.appendChild(style);
+
