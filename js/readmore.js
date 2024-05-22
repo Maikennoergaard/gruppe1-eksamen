3ece901c154cd40b6fb3ff31667a0a4e630a7888
Anvinage <eaa23avi@students.eaaa.dk>
2024-05-22 02:23:08 +0200

    Head + kommentarer i java script

--- js/readmore.js
+++ js/readmore.js
@@ -1,27 +1,32 @@
 'use strict';
 
+// Når DOM'en er indlæst, udføres følgende:
 document.addEventListener("DOMContentLoaded", function() {
   var foldReplyElements = document.querySelectorAll(".fold_reply");
 
+ // For hvert "fold_reply" element, tilføj en klikhændelse
   foldReplyElements.forEach(function(foldReply) {
       foldReply.addEventListener("click", function() {
-          var targetClass = foldReply.getAttribute("data-target");
-          var targetElement = document.querySelector(targetClass);
 
-          // gemmer de andre reply sektioner
-          document.querySelectorAll(".reply").forEach(function(reply) {
+            // Hent værdien af "data-target" attributten for det klikkede element
+            var targetClass = foldReply.getAttribute("data-target");
+
+            // Find det element, der har klassen, der er angivet i "data-target"
+            var targetElement = document.querySelector(targetClass);
+
+            // Gem alle andre svarsektioner ved at fjerne klassen "visible"
+            document.querySelectorAll(".reply").forEach(function(reply) {
               if (reply !== targetElement) {
                   reply.classList.remove("visible");
               }
           });
 
-          
           targetElement.classList.toggle("visible");
       });
   });
 });
 
-// Ekstar CSS til styling
+// Ekstra CSS til styling
 const style = document.createElement('style');
 style.innerHTML = `
     .reply {
